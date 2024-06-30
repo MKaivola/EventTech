757439871230eb6edf8650f3eda7132f02540d8f
Mikko Kaivola <mkaivola96@gmail.com>
2024-06-30 15:46:09 +0300

    Period table
    - Added a period table which contains the start and end date of each fiscal year
    - Changed DateTime attributes to Date type

--- Data/db_metadata.py
+++ Data/db_metadata.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Table, Column, MetaData, ForeignKey, Insert, Select
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, String, Date
 import pandas as pd
 
 class EventDataBase():
@@ -14,17 +14,20 @@ class EventDataBase():
                            self.metadata,
                            Column('id_name', Integer, primary_key=True),
                            Column('name_tech', String))
+        
         self.events = Table('Events',
                             self.metadata,
                             Column('id_event', Integer, primary_key=True),
                             Column('name_event', String),
-                            Column('date_event', DateTime),
+                            Column('date_event', Date),
                             Column('location_event', String),
                             Column('description_event', String))
+        
         self.jobs = Table('Jobs',
                           self.metadata,
                           Column('id_job', Integer, primary_key=True),
                           Column('name_job', String))
+        
         self.signups = Table('Signups',
                              self.metadata,
                              Column('id_signup', Integer, primary_key=True),
@@ -32,6 +35,13 @@ class EventDataBase():
                              Column('job_id', Integer, ForeignKey('Jobs.id_job')),
                              Column('name_id', Integer, ForeignKey('Names.id_name')),
                              Column('answer', String))
+        
+        self.periods = Table('Periods',
+                             self.metadata,
+                             Column('id_period', Integer, primary_key=True),
+                             Column('period_name', String),
+                             Column('start_date', Date),
+                             Column('end_date', Date))
     
     def _create_tables(self, df_names: pd.DataFrame,
                        df_events: pd.DataFrame,
@@ -49,3 +59,26 @@ class EventDataBase():
             for db_data, db in db_data_pairs:
                 conn.execute(Insert(db),
                              db_data.to_dict(orient='records'))
+                
+        with self.engine.begin() as conn:
+            conn.execute(Insert(self.periods),
+                         [{
+                            'id_period': 1, 
+                            'period_name': '2021-2022', 
+                            'start_date': pd.Timestamp(year=2021,
+                                                        month=7,
+                                                        day=1),
+                            'end_date': pd.Timestamp(year=2022,
+                                                     month=6,
+                                                     day=30)
+                         },
+                         {
+                            'id_period': 2,
+                            'period_name': '2022-2023',
+                            'start_date': pd.Timestamp(year=2022,
+                                                        month=7,
+                                                        day=1),
+                            'end_date': pd.Timestamp(year=2023,
+                                                     month=6,
+                                                     day=30)
+                         }])
