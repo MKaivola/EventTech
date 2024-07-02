069b9acd1949f409f095d4e4716a6606f1b9edf4
Mikko Kaivola <mkaivola96@gmail.com>
2024-07-02 19:50:14 +0300

    Refactoring analysis_func
    - Various processing steps have been refactored to separate functions
    - New module utils_analysis contains utility functions

--- analysis_func.py
+++ analysis_func.py
@@ -5,6 +5,7 @@ from sqlalchemy import Select, bindparam
 from sqlalchemy import func
 
 from Data.db_metadata import EventDataBase
+import utils_analysis
 
 def monthly_event_counts(db: EventDataBase,
                          period_names: Iterable[str]) -> pd.DataFrame:
@@ -34,48 +35,33 @@ def monthly_event_counts(db: EventDataBase,
                             )
     
     # Select relevant periods
-    periods_stmt = (Select(db.periods.c['period_name', 'start_date', 'end_date'])
-                    .where(db.periods.c.period_name.in_(period_names)))
+    periods = utils_analysis.get_periods(db,
+                                         period_names)
 
-    with db.engine.begin() as conn:
-        periods = pd.read_sql(periods_stmt,
-                            conn)
-        
-        # Extract counts for each fiscal period
-        events_per_month_list = [pd.read_sql(events_per_month_stmt,
-                                                     conn,
-                                                     params={
-                                                         'start_date': row['start_date'],
-                                                         'end_date': row['end_date']
-                                                     }) for _, row in periods.iterrows()]
-        
-        events_per_month = pd.concat(events_per_month_list)
-        
+    events_per_month = utils_analysis.get_and_concat_periods(db,
+                                                             events_per_month_stmt,
+                                                             periods)
 
     # Construct all monthly periods for each fiscal year
-    periods['Month'] = periods.apply(lambda row: pd.period_range(start=row['start_date'],
-                                                                end=row['end_date'],
-                                                                freq='M'),
-                                    axis=1)
-
-    periods = periods.explode('Month').drop(columns=['start_date',
-                                                    'end_date'])
+    periods = utils_analysis.generate_pd_periods(periods,
+                                                 'M')
 
     # Construct period objects for each month year pair
-    periods_in_data = pd.to_datetime(events_per_month.drop(columns='event_count').assign(day = 1)).dt.to_period('M')
+    periods_in_data = (pd.to_datetime(events_per_month.drop(columns='event_count')
+                                      .assign(day = 1)).dt.to_period('M'))
 
     events_per_month = (events_per_month.assign(period = periods_in_data)
                         .loc[:, ['period', 'event_count']].set_index('period'))
 
     # Join all periods with periods containing data
-    event_counts = periods.join(events_per_month, on='Month').fillna(0)
+    event_counts = periods.join(events_per_month, on='Period').fillna(0)
 
-    event_counts['Month'] = event_counts['Month'].dt.month
+    event_counts['Month'] = event_counts['Period'].dt.month
 
     # Pivot so that each fiscal year has a separate column
     event_counts = (event_counts.pivot(index='Month',
                                     columns='period_name',
                                     values='event_count')
-                                    .rename_axis(columns='Period'))
+                                    .rename_axis(columns='Fiscal Year'))
     
     return event_counts
---
+++ utils_analysis.py
@@ -0,0 +1,81 @@
+from collections.abc import Iterable
+
+import pandas as pd
+from sqlalchemy import Select
+
+from Data.db_metadata import EventDataBase
+
+def get_periods(db: EventDataBase,
+                period_names: Iterable[str]) -> pd.DataFrame:
+    """
+    Get fiscal period table for given periods
+
+    Arguments
+    ---------
+    db:
+        A database object
+    period_names:
+        An iterable of periods to get by period name
+    """
+
+    # Select relevant periods
+    periods_stmt = (Select(db.periods.c['period_name', 'start_date', 'end_date'])
+                    .where(db.periods.c.period_name.in_(period_names)))
+
+    with db.engine.begin() as conn:
+        periods = pd.read_sql(periods_stmt,
+                            conn)
+        
+    return periods
+
+def generate_pd_periods(periods: pd.DataFrame,
+                        freq: str) -> pd.DataFrame:
+    """
+    Generate pandas Period objects spanning each fiscal year for a given frequency
+
+    Arguments
+    ---------
+    periods:
+        A dataframe containing each period and its start and end dates
+    freq:
+        A string specifying the frequency of periods
+    """
+
+    df = periods.assign(Period = periods.apply(lambda row: pd.period_range(start=row['start_date'],
+                                                                            end=row['end_date'],
+                                                                            freq=freq),
+                                                axis=1))
+
+    df = df.explode('Period').drop(columns=['start_date',
+                                            'end_date'])
+    
+    return df
+
+def get_and_concat_periods(db: EventDataBase,
+                           stmt: Select,
+                           periods: pd.DataFrame) -> pd.DataFrame:
+    """
+    Get and concatenate data that is extracted for each period separately
+
+    Arguments
+    ---------
+    db:
+        A database object
+    stmt:
+        A SQLAlchemy Select object to be executed
+    periods:
+        A dataframe containing each period and its start and end dates
+    """
+    
+    with db.engine.begin() as conn:        
+        # Extract data for each fiscal period
+        data_for_each_period = [pd.read_sql(stmt,
+                                            conn,
+                                            params={
+                                                'start_date': row['start_date'],
+                                                'end_date': row['end_date']
+                                            }) for _, row in periods.iterrows()]
+        
+        all_data = pd.concat(data_for_each_period)
+
+    return all_data
