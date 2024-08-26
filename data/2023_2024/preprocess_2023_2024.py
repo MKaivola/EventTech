7e9081e5947840f55910459462175606770d0454
Mikko Kaivola <mkaivola96@gmail.com>
2024-08-26 18:53:32 +0300

    More events included for 2023-2024 data

--- data/2023_2024/preprocess_2023_2024.py
+++ data/2023_2024/preprocess_2023_2024.py
@@ -14,6 +14,7 @@ flat_polls_general_chat = utils.extract_flat_event_polls(
         "kasa": "Kasaus",
         "pur": "Purku",
         "aja": "Veto",
+        "ajo": "Veto",
         "veto": "Veto",
         "ei": "En pääse",
         "en": "En pääse",
@@ -25,9 +26,12 @@ utils.update_event_names(flat_polls_general_chat)
 
 event_data_2023_2024 = pd.DataFrame(flat_polls_general_chat)
 
-event_data_2023_2024 = event_data_2023_2024.drop(
-    index=event_data_2023_2024[event_data_2023_2024["name_event"] == "Smökrok"].index
-)
+events_to_drop = ["Smökrok", "Orientaatioviikon keikat (kaikki Smökissä)"]
+
+is_event_to_drop = event_data_2023_2024["name_event"].isin(events_to_drop)
+
+index_to_drop = event_data_2023_2024.loc[is_event_to_drop, :].index
+
+event_data_2023_2024 = event_data_2023_2024.drop(index=index_to_drop)
 
 event_data_2023_2024.to_csv(data_path / "Tapahtumat_2023_2024.csv", index=False)
-
